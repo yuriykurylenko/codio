294faa9fa7170279a835bf3f8c70388138c61c14
Yuriy <yukur@ukr.net>
2013-09-21 22:45:49 +0300

    Basic display of contacts

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,28 @@
 var codioApp = angular.module('Codio', []);
 
-codioApp.controller('TeamCreateCtrl', ['$scope', function($scope) {
+
+codioApp.service('contactsService', ['$http', function($http) {
+    return {
+        get: function(callback) {
+        	if (_.isFunction(callback)) {
+	            $http(
+	                { method: 'GET', url: '/contacts' }
+	            ).success(function(data) {
+	                callback(data);
+	            }).error(function(html, statusCode) {
+	                throw new Error('"/contacts" responded with status code ' + statusCode);
+	            });
+	        } else {
+	        	throw new Error('contactsService.get(): first parameter should be a function!');
+	        }
+        }
+    }
+}]);
+
+codioApp.controller('TeamCreateCtrl', ['$scope', 'contactsService', function($scope, contactsService) {
 	$scope.text = 'It works!';
+
+	contactsService.get(function(data) {
+		$scope.contacts = data;
+	});
 }]);
