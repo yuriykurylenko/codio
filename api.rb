41fd0da1e25a60cbad975ad3a5b3d98d535319f2
Yuriy <yukur@ukr.net>
2013-09-23 00:44:08 +0300

    Indents converted to spaces

--- api.rb
+++ api.rb
@@ -7,44 +7,44 @@ require 'json'
 set :public_folder, 'public'
 
 def podio
-	Podio.setup(:api_key => 'yuriys-test-app', :api_secret => 'tcq960QPhD7agosMnOcMcp4FzjlWDkt4TY5T3crNUpFBmKF9Txu8Y8jHLPa8vsOc')
-	Podio.client.authenticate_with_credentials('yukur@ukr.net', 'Godfather1')
+    Podio.setup(:api_key => 'yuriys-test-app', :api_secret => 'tcq960QPhD7agosMnOcMcp4FzjlWDkt4TY5T3crNUpFBmKF9Txu8Y8jHLPa8vsOc')
+    Podio.client.authenticate_with_credentials('yukur@ukr.net', 'Godfather1')
 end
 
 get '/' do
-	File.read(File.join('public', 'index.html'))
+    File.read(File.join('public', 'index.html'))
 end
 
 get '/orgs' do 
-	podio
-	my_orgs = Podio::Organization.find_all
+    podio
+    my_orgs = Podio::Organization.find_all
 
-	my_orgs.to_json
+    my_orgs.to_json
 end
 
 get '/contacts' do
-	podio
-	my_contacts = Podio::Contact.all
+    podio
+    my_contacts = Podio::Contact.all
 
-	my_contacts.to_json	
+    my_contacts.to_json 
 end
 
 get '/me' do
-	podio
-	me = Podio::User.current
+    podio
+    me = Podio::User.current
 
-	me.to_json
+    me.to_json
 end
 
 post '/workspace' do
-	podio
+    podio
 
-	json_data = JSON.parse request.body.read
+    json_data = JSON.parse request.body.read
 
-	result = Podio::Space.create( 
-		:name => json_data["name"], 
-		:org_id => json_data["org_id"]
-	)
+    result = Podio::Space.create( 
+        :name => json_data["name"], 
+        :org_id => json_data["org_id"]
+    )
 
-	result.to_json
+    result.to_json
 end
--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,18 @@ var codioApp = angular.module('Codio', []);
 codioApp.service('contactsService', ['$http', function($http) {
     return {
         get: function(callback) {
-        	if (!_.isFunction(callback)) {
-				throw new Error('contactsService.get(): first parameter should be a Function!');
-        	}
+            if (!_.isFunction(callback)) {
+                throw new Error('contactsService.get(): first parameter should be a Function!');
+            }
 
-        	
-	        $http(
-	            { method: 'GET', url: '/contacts' }
-	        ).success(function(data) {
-	            callback(data);
-	        }).error(function(html, statusCode) {
-	            throw new Error('"/contacts" responded with status code ' + statusCode);
-	        });
+            
+            $http(
+                { method: 'GET', url: '/contacts' }
+            ).success(function(data) {
+                callback(data);
+            }).error(function(html, statusCode) {
+                throw new Error('"/contacts" responded with status code ' + statusCode);
+            });
         }
     }
 }]);
@@ -24,69 +24,69 @@ codioApp.service('contactsService', ['$http', function($http) {
 codioApp.service('orgsService', ['$http', function($http) {
     return {
         get: function(callback) {
-        	if (!_.isFunction(callback)) {
-				throw new Error('orgsService.get(): first parameter should be a Function!');
-        	}
+            if (!_.isFunction(callback)) {
+                throw new Error('orgsService.get(): first parameter should be a Function!');
+            }
 
-        	
-	        $http(
-	            { method: 'GET', url: '/orgs' }
-	        ).success(function(data) {
-	            callback(data);
-	        }).error(function(html, statusCode) {
-	            throw new Error('"/orgs" responded with status code ' + statusCode);
-	        });
+            
+            $http(
+                { method: 'GET', url: '/orgs' }
+            ).success(function(data) {
+                callback(data);
+            }).error(function(html, statusCode) {
+                throw new Error('"/orgs" responded with status code ' + statusCode);
+            });
         }
     }
 }]);
 
 codioApp.service('wspService', ['$http', function($http) {
-	return {
-		post: function(data, callback) {
-	    	if (!_.isFunction(callback)) {
-				throw new Error('wspService.post(): second parameter should be a Function!');
-	    	}
+    return {
+        post: function(data, callback) {
+            if (!_.isFunction(callback)) {
+                throw new Error('wspService.post(): second parameter should be a Function!');
+            }
 
-		    $http(
-		        { method: 'POST', url: '/workspace', data: data }
-		    ).success(function(data) {
-		        callback(data);
-		    }).error(function(html, statusCode) {
-		        throw new Error('"/orgs" responded with status code ' + statusCode);
-		    });
-		}
-	}	
+            $http(
+                { method: 'POST', url: '/workspace', data: data }
+            ).success(function(data) {
+                callback(data);
+            }).error(function(html, statusCode) {
+                throw new Error('"/orgs" responded with status code ' + statusCode);
+            });
+        }
+    }   
 }]);
 
 codioApp.controller('TeamCreateCtrl', 
 ['$scope', 'contactsService', 'orgsService', 'wspService', function($scope, contactsService, orgsService, wspService) {
-	$scope.team = [];
+    $scope.team = [];
 
-	contactsService.get(function(data) {
-		$scope.contacts = data;
-	});
+    contactsService.get(function(data) {
+        $scope.contacts = data;
+    });
 
-	orgsService.get(function(data) {
-		$scope.orgs = data;
-	});
+    orgsService.get(function(data) {
+        $scope.orgs = data;
+    });
 
-	$scope.activate = function(contactId) {
-		// No DOM manipulations in controllers!
-		$('#contact' + contactId + ' > div').toggleClass('alert-success');
-		if ($scope.team.indexOf(contactId) == -1) {
-			$scope.team.push(contactId);
-		} else {
-			$scope.team = _.without($scope.team, contactId);
-		}
-	}
+    $scope.activate = function(contactId) {
+        // No DOM manipulations in controllers!
+        $('#contact' + contactId + ' > div').toggleClass('alert-success');
+        if ($scope.team.indexOf(contactId) == -1) {
+            $scope.team.push(contactId);
+        } else {
+            $scope.team = _.without($scope.team, contactId);
+        }
+    }
 
-	$scope.create = function() {
-		wspService.post({
-			name: $scope.name, 
-			org_id: Number($scope.organization)
-			//, $scope.team
-		}, function(data) {
-			console.log(data);
-		});
-	}
+    $scope.create = function() {
+        wspService.post({
+            name: $scope.name, 
+            org_id: Number($scope.organization)
+            //, $scope.team
+        }, function(data) {
+            console.log(data);
+        });
+    }
 }]);
