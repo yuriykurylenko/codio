5e92f685aa0c55328d7b3b7ff1cdfad286528fed
Yuriy <yukur@ukr.net>
2013-09-22 17:40:34 +0300

    Ability to select users added

--- public/js/app.js
+++ public/js/app.js
@@ -4,25 +4,40 @@ var codioApp = angular.module('Codio', []);
 codioApp.service('contactsService', ['$http', function($http) {
     return {
         get: function(callback) {
-        	if (_.isFunction(callback)) {
-	            $http(
-	                { method: 'GET', url: '/contacts' }
-	            ).success(function(data) {
-	                callback(data);
-	            }).error(function(html, statusCode) {
-	                throw new Error('"/contacts" responded with status code ' + statusCode);
-	            });
-	        } else {
-	        	throw new Error('contactsService.get(): first parameter should be a function!');
-	        }
+        	if (!_.isFunction(callback)) {
+				throw new Error('contactsService.get(): first parameter should be a Function!');
+        	}
+
+        	
+	        $http(
+	            { method: 'GET', url: '/contacts' }
+	        ).success(function(data) {
+	            callback(data);
+	        }).error(function(html, statusCode) {
+	            throw new Error('"/contacts" responded with status code ' + statusCode);
+	        });
         }
     }
 }]);
 
 codioApp.controller('TeamCreateCtrl', ['$scope', 'contactsService', function($scope, contactsService) {
-	$scope.text = 'It works!';
+	$scope.team = [];
 
 	contactsService.get(function(data) {
 		$scope.contacts = data;
 	});
+
+	$scope.activate = function(contactId) {
+		// No DOM manipulations in controllers!
+		$('#contact' + contactId + ' > div').toggleClass('alert-success');
+		if ($scope.team.indexOf(contactId) == -1) {
+			$scope.team.push(contactId);
+		} else {
+			$scope.team = _.without($scope.team, contactId);
+		}
+	}
+
+	$scope.create = function() {
+		alert($scope.team);
+	}
 }]);
